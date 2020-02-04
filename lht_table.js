d6176ca8d24cfcd7390d890f6894ec0ef0025b52
Gael Rodriguez Garcia <grodriguez236@west-mec.org>
2020-02-03 18:34:41 -0700

    partly done

---
+++ lht_table.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Gael Rodriguez Garcia 
+   Date:  2/3/20 
+
+	
+*/
+//creating global variables
+var thisDay = new Date("August 30, 2018");
+var tableHTML = "<table id='eventTable'><caption>Upcoming Events</captions><tr><th>Date</th><th>Event</th><th>price</th></tr>";
+var endDate = new Date("thisDay.getTime()+ 14 * 24 * 60 * 60 * 1000");
+
+// creating loop for calendar
+for(var i = 0; i < eventDates.length; i++){
+    var eventDate = new Date(eventDates[i]);
+    var eventDay = eventDate.toDateString();
+    var eventTime = eventDate.toLocaleTimeString();
+//if statement with conditional expression   
+    if(thisDay <= eventDate && eventDate <= endDate){
+        tableHTML += "<tr>";
+        tableHTML += "<td>" + eventDay + "@" + eventTime + "</td>";
+        tableHTML += "<td>" + description + "</td>";
+        tableHTML += "<td>" + eventPrices[i] + "</td>";
+        tableHTML += "</tr>";
+    }
+
+}
+tableHTML += "</table>";
+
+
+
